4330a4d4f60063f508b6e3522897d1de6ed4e786
lhoriman <lhoriman@2d369ceb-f315-3f27-9fc4-66eee17cf1f6>
2010-09-26 01:48:04 +0000

    Improvements to client, making request to server now

--- client/trunk/src/meatography.py
+++ client/trunk/src/meatography.py
@@ -1,38 +1,83 @@
+#
+# Script which controlls a meatography cabinet, submitting temperature and humidity values
+# to a server and actuating control devices to maintain stable values.
+#
+# This script should be run from cron once per minute.
+#
+# It reads a config file called meatography.cfg which is expected to look like this:
+#
+# [meatography]
+# cabinet_id=nameOfYourCabinet	# name of your cabinet in the server
+# owserver=localhost:4304	# how to access the onewire server; could be "u" for usb directly
+# server_url=http://www.meatography.com/submit	# url of the server
+
+import urllib
 import urllib2
 import logging
-from datetime import datetime
+import datetime
 import time
 import ow
+import ConfigParser
 
-config = {}
-
-# Configure this with a unique id for the cabinet
-config["cabinet_id"] = "cabinet0"
-#config["owserver"] = "u"	# for direct USB access, don't run the server
-
-# Access to the onewire server
-config["owserver"] = "localhost:4304"
-
-# Base url to the server
-config["server_url"] = "http://www.meatography.com/submit"
+# The defaults - change your local settings in a meatography.cfg file
+cfg_section = "meatography"
+cfg_owserver_param = "owserver"
+cfg_server_url_param = "server_url"
+cfg_cabinet_id_param = "cabinet_id"
+cfg_defaults = {
+			cfg_owserver_param: "localhost:4304",
+			cfg_server_url_param: "http://www.meatography.com/submit"
+			}
 
 # Log if you want it
 logging.getLogger().setLevel(logging.DEBUG)
 
 #
+def get_temp_sensor():
+	#return ow.Sensor("/10.FF09E6010800")
+	tempSensors = ow.Sensor("/").find(family="10")
+	if not tempSensors:
+		raise Exception, "Unable to find temperature sensor"
+	elif len(tempSensors) > 1:
+		raise Exception, "Too many temperature sensors on bus"
+	else:
+		return tempSensors[0]
+
+#
+def get_humidity_sensor():
+	#return ow.Sensor("/26.C9C9F1000000")
+	humiditySensors = ow.Sensor("/").find(family="26")
+	if not humiditySensors:
+		raise Exception, "Unable to find humidity sensor"
+	elif len(humiditySensors) > 1:
+		raise Exception, "Too many humidity sensors on bus"
+	else:
+		return humiditySensors[0]
+
+	
+#
 # Let the program continue
 #
-ow.init(config["owserver"])
-sensTemp = ow.Sensor("/10.FF09E6010800")
-sensHumid = ow.Sensor("/26.C9C9F1000000")
+cfg = ConfigParser.ConfigParser(cfg_defaults)
+ow.init(cfg.get(cfg_section, cfg_owserver_param))
+tempSensor = get_temp_sensor()
+humiditySensor = get_humidity_sensor()
 
-temp = float(sensTemp.temperature)
-humidity = float(sensHumid.humidity)
+# Get the real values
+temp = float(tempSensor.temperature)
+humidity = float(humiditySensor.humidity)
 
-logging.info(u"Temp is {0}\u00b0C".format(temp))
+logging.info(u"Temperature is {0}\u00b0C".format(temp))
 logging.info(u"Humidity is {0}%".format(humidity))
 
+# Make the call to the server
 params = {}
 params["ver"] = 1
-params["cid"] = config["cabinet_id"]
-params["when"] = time.mktime(datetime.now().timetuple()) * 1000	# java time format
+params["cid"] = cfg.get(cfg_section, cfg_cabinet_id_param)
+params["when"] = time.mktime(datetime.datetime.now().timetuple()) * 1000	# java time format
+params["temp"] = temp
+params["humidity"] = humidity
+
+encoded_params = urllib.urlencode(params)
+
+response = urllib2.urlopen(cfg.get(cfg_section, cfg_server_url_param), encoded_params)
