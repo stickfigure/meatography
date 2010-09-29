7e0cda98e99b568acc4d1f5c1d2d9c9853f3023c
scotthernandez <scotthernandez@2d369ceb-f315-3f27-9fc4-66eee17cf1f6>
2010-09-29 01:31:47 +0000

    added /meat/test to return json

--- server/trunk/meat/handlers.py
+++ server/trunk/meat/handlers.py
@@ -2,21 +2,20 @@ from entities import Measurement
 from moe.base.handlers import AreaRequestHandler
 from wtforms.ext.appengine.db import model_form
 from wtforms.form import Form
+from pytz import UTC
+from datetime import datetime
+from tipfy import render_json_response
 
-class MainPage(AreaRequestHandler):
-    def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.write('Hello, webapp2 World!')
-        
 class NewMeasurement(AreaRequestHandler):
     '''
     Creates a new measurement from a post/get
     '''
     def createNew(self):
         m = Measurement()
-        m_form = model_form(Measurement)
+        m_form = model_form(Measurement, exclude=("when"))
         assert isinstance(m_form, Form)
         m_form.populate_obj(m)
+        m.when = datetime.datetime.fromtimestamp(int(self.request.get("when")), UTC)
         m.save()
         
     def get(self):
@@ -26,6 +25,10 @@ class NewMeasurement(AreaRequestHandler):
         self.createNew()
         return self.redirect_to("measurements", 302)
 
+class ResultStub(AreaRequestHandler):
+    def get(self):
+        return render_json_response({ "status":0, "tempTarget": 50, "humidityTarget": 70 })
+    
 class ListRawMeasurements(AreaRequestHandler):
     '''
     List all the measurements
--- server/trunk/meat/urls.py
+++ server/trunk/meat/urls.py
@@ -9,6 +9,7 @@ def get_rules():
         Rule('/', endpoint='measurements-index', handler='meat.handlers.ListRawMeasurements'),
         Rule('/measurements', endpoint='measurements', handler='meat.handlers.ListRawMeasurements'),
         Rule('/submit', endpoint='new-measurement', handler='meat.handlers.NewMeasurement'),
+        Rule('/test', endpoint='new-measurement', handler='meat.handlers.ResultStub'),
     ]
 
     return rules
