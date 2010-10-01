7981cca9ad995de10d27e8bbcaea9fd560757ef1
lhoriman <lhoriman@2d369ceb-f315-3f27-9fc4-66eee17cf1f6>
2010-10-01 07:36:09 +0000

    So main can be run again and again

--- client/trunk/src/x10/test.py
+++ client/trunk/src/x10/test.py
@@ -1,5 +1,6 @@
 import serial
 
+#tty = "/dev/tty.usbserial"
 tty = "/dev/ttyUSB0"
 #tty = "/dev/ttyS1"
 
@@ -10,14 +11,17 @@ unit = 0x4c
 cmd = 0x45
 repeat = 0x41
 
-port = serial.Serial(tty, 9600, timeout=1)
-port.open()
-result = port.read()
-print "Initial crap: '" + result + "', length=" + str(len(result)) 
-port.write(chr(0x02))
-
-result = port.read()
-print "After write: '" + result + "', length=" + str(len(result))
-print "Firstchar is '" + hex(ord(result[0])) + "'"
-
-port.close()
+def main():
+	port = serial.Serial(tty, 9600, timeout=1)
+	port.open()
+	result = port.read()
+	print "Initial crap: '" + result + "', length=" + str(len(result)) 
+	
+	port.write(chr(0x02))
+	result = port.read()
+	print "After write: '" + result + "', length=" + str(len(result))
+	print "Firstchar is '" + hex(ord(result[0])) + "'"
+	
+	port.close()
+	
+main()
